4753f0d90ac39d670f2d8ca0aa8326f5f662ed58
SunaUezuri <wesley.jane.santos@gmail.com>
2025-09-25 18:18:23 -0300

    Criando a service de Filial com controle de acesso para administradores

---
+++ src/main/java/br/com/geomottu/api/services/FilialService.java
@@ -0,0 +1,66 @@
+package br.com.geomottu.api.services;
+
+import br.com.geomottu.api.config.security.SecurityUtils;
+import br.com.geomottu.api.dto.filial.FilialDto;
+import br.com.geomottu.api.dto.filial.FilialGetDto;
+import br.com.geomottu.api.exceptions.IdNaoEncontradoException;
+import br.com.geomottu.api.model.entities.Filial;
+import br.com.geomottu.api.repository.FilialRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FilialService {
+
+    private final FilialRepository filialRepository;
+    private final SecurityUtils securityUtils;
+
+    public Filial save(FilialDto dto) {
+        securityUtils.checkAdminAccess();
+
+        Filial filial = new Filial(dto);
+
+        return filialRepository.save(filial);
+    }
+
+    public List<FilialGetDto> getAll() {
+        securityUtils.checkAdminAccess();
+        return filialRepository.findAll()
+                .stream()
+                .map(FilialGetDto::new)
+                .toList();
+    }
+
+    public FilialGetDto getById(Long id) throws IdNaoEncontradoException {
+        securityUtils.checkAdminAccess();
+        return filialRepository.findById(id)
+                .map(FilialGetDto::new)
+                .orElseThrow(() -> new IdNaoEncontradoException("Filial não encontrada com ID: " + id));
+    }
+
+    public FilialGetDto update(Long id, FilialGetDto dto) throws IdNaoEncontradoException {
+        securityUtils.checkAdminAccess();
+        Filial filial = filialRepository.findById(id)
+                .orElseThrow(() -> new IdNaoEncontradoException("Filial não encontrada com ID: " + id));
+
+        filial.setNome(dto.nome());
+        filial.setPais(dto.pais());
+        filial.setEndereco(dto.endereco());
+        filial.setTelefone(dto.telefone());
+        filial.setEmail(dto.email());
+
+        Filial atualizado = filialRepository.save(filial);
+
+        return new FilialGetDto(atualizado);
+    }
+
+    public void delete(Long id) throws IdNaoEncontradoException {
+        Filial filial = filialRepository.findById(id)
+                .orElseThrow(() -> new IdNaoEncontradoException("Filial não encontrada com ID: " + id));
+
+        filialRepository.delete(filial);
+    }
+}
